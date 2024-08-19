53eb8fea5f3455240fd78c8db612570c4085d61d
Ahmed Utku ÖZÜDOĞRU <ahmedutkuozudogru@gmail.com>
2024-08-19 17:17:20 +0300

    Db error log clarification

--- backend/db/connectDB.js
+++ backend/db/connectDB.js
@@ -5,7 +5,7 @@ export const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URI)
     console.log(`MongoDB Connected: ${conn.connection.host}`)
   } catch (error) {
-    console.error(`Error: ${error.message}`)
+    console.error(`DB Error : ${error.message}`)
     process.exit(1) // 1 means exit with failure , 0 means exit with success
   }
 }
